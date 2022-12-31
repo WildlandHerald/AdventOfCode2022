b07134a04652f5de0a72aafb9d1086d135bacbb3
GamerZero <WildlandHerald@mail.ru>
2022-12-31 04:43:28 +0300

    Add solution for Day 4

---
+++ src/Day4.fs
@@ -0,0 +1,30 @@
+﻿module Day4
+
+open Utils
+open System.IO
+
+let data = File.ReadAllLines("../../../inputs/day4.txt") |> Array.toList
+
+let rangesContain ranges =
+    match ranges with
+    | [ [ x1; y1 ]; [ x2; y2 ] ] -> (x1 >= x2 && y1 <= y2) || (x1 <= x2 && y1 >= y2)
+    | _ -> invalidArg "ranges" "ranges were provided in incorrect format"
+
+let solution test input =
+    input
+    |> List.filter (
+        String.split ","
+        >> List.map (String.split "-")
+        >> List.map (List.map int)
+        >> test
+    )
+    |> List.length
+
+printfn "%A" (solution rangesContain data)
+
+let rangesOverlap ranges =
+    match ranges with
+    | [ [ x1; y1 ]; [ x2; y2 ] ] -> x1 <= y2 && y1 >= x2
+    | _ -> invalidArg "ranges" "ranges were provided in incorrect format"
+
+printfn "%A" (solution rangesOverlap data)
